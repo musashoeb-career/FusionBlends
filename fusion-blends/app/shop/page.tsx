6e645ed54060b57ade34d901fc6270d8c5507a63
musashoeb <mshoeb@umich.edu>
2025-05-30 17:08:38 -0400

    Update Shop Page

--- fusion-blends/app/shop/page.tsx
+++ fusion-blends/app/shop/page.tsx
@@ -1,9 +1,37 @@
+import Image from "next/image";
 
 export default function ShopPage() {
   return (
     <main>
-      <div className= "min-h-screen flex justify-center items-center text-white text-2xl">
-       This is the shop page
+      <div className= "min-h-screen flex justify-center items-center text-2xl">
+        <div className="grid grid-cols-3 gap-x-10 ">
+           <div className="flex flex-col rounded-[15px] w-[300px] h-[500px] bg-white drop-shadow-2xl">
+            <div className="flex-1 ">
+              <Image src={"/pressed-logo-pink.png"} alt = "Pink Juice Bottle" objectFit="contain" width={500} height = {2000}></Image>
+            </div>
+            <div className="flex-1 flex flex-col items-center">
+               <div className="subheading text-3xl m-[10px]">
+              Lankan Looda
+            </div>
+            <div className="text-[14px] font-[100] m-[15px]">
+              FALOODA IS A POPULAR SOUTH ASIAN SWEET, 
+              CREAMY DRINK MADE WITH ROSE SYRUP AND MILK, 
+              OFFERING A FRAGRANT AND REFRESHING FLAVOR.
+            </div>
+            </div>
+          </div>
+           <div className=" flex flex-col rounded-lg w-[300px] h-[500px] bg-white">
+             <div className="flex-1 ">
+              <Image src={"/pressed-logo-brown.png"} alt = "Brown Juice Bottle" objectFit="contain" width={500} height = {2000}></Image>
+            </div>
+          </div>
+           <div className="rounded-lg w-[300px] h-[500px] bg-white">
+              <div className="flex-1 ">
+              <Image src={"/pressed-logo-green.png"} alt = "Green Juice Bottle" objectFit="contain" width={500} height = {2000}></Image>
+            </div>
+          </div>
+        </div>
+        
       </div>
 
     </main>
--- fusion-blends/components/navbar.tsx
+++ fusion-blends/components/navbar.tsx
@@ -6,16 +6,16 @@ export default function NavBar() {
     return(
         <div className="flex">
             
-            <div className="relative rounded-lg w-[150px] h-[155px] m-[15px] p-[20px] bg-white">
+            <div className="relative rounded-lg w-[150px] h-[150px] m-[15px] bg-white">
                 <Image src = {"/fusion-logo.png"} alt = "Fusion Blends Logo" objectFit="contain" layout="fill"></Image>
             </div>
-           <nav className="flex-6 flex justify-around items-center mt-[20px] text-white">
+           <nav className="flex-6 flex justify-around items-center text-[var(--ivory)]">
             <Link href = "/" className="hover:brightness-75">Home</Link>
              <Link href = "/about" className="hover:brightness-75">About</Link>
              <Link href = "/shop" className="hover:brightness-75">Shop</Link>
              <Link href = "/contact" className="hover:brightness-75">Contact</Link>
            </nav>
-           <div className="flex-1 flex justify-around items-center mt-[20px] w-1/8"> 
+           <div className="flex-1 flex justify-around items-center w-1/8"> 
               <Image src={"user.svg"} alt="User Profile Icon" width={25} height={25} />
 
                <Image src={"shopping.svg"} alt="Shopping Cart Icon" width={25} height={25} />
